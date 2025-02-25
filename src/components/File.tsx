381b2ade519863faef4fc6f6014c4e8ee8210dbb
oMarquess <olufemimarquess@gmail.com>
2025-02-25 01:41:15 +0000

    Image file acceptance

--- src/components/File.tsx
+++ src/components/File.tsx
@@ -63,6 +63,8 @@ export function FileForm() {
         accept: {
             'application/pdf': ['.pdf'],
             'application/msword': ['.doc', '.docx'],
+            'image/jpeg': ['.jpg', '.jpeg'],
+            'image/png': ['.png'],
         },
     });
 
--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 export const API_ENDPOINT = 'https://meducate-4b014c640ca1.herokuapp.com/labs/interpret' ;
+// export const API_ENDPOINT = "http://127.0.0.1:8000/labs/interpret"
 
